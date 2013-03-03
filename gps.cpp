9d558bf95513b4e3a1f50fa586ccecd2c02c4974
Austin Hendrix <legotown@aol.com>
2013-03-03 11:22:10 -0800

    Publish GPS location to tablet.

--- gps.cpp
+++ gps.cpp
@@ -19,12 +19,34 @@ uint8_t gps_port;
 //ros::Publisher gps_pub("gps", &gps_msg);
 //char gps_frame[] = "";
 Publisher<16> gps_pub('G');
+Publisher<16, BT> gps_pub2('G');
 
 /* initialize GPS listener on serial port */
 void gps_init(uint8_t port) {
    // initialize serial port and set baud rate
    serial_init(port);
-   serial_baud(port, 9600);
+   serial_baud(port, 57600);
+
+   // set GPS to 57600 baud
+   /* uint8_t gps_initstr[] = { 
+      0xA0, 0xA1, // header
+      0x00, 0x04, // length
+      0x05, 0x00, 0x04, 0x01, // payload
+      0x00, // checksum
+      0x0D, 0x0A // tail
+   }; */
+   // set GPS to 5 Hz
+   uint8_t gps_initstr[] = { 
+      0xA0, 0xA1, // header
+      0x00, 0x03, // length
+      0x0E, 0x05, 0x00, // payload
+      0x0B, // checksum 1110 ^ 0101 = 1011 (b
+      0x0D, 0x0A // tail
+   };
+   volatile uint16_t gps_len = sizeof(gps_initstr);
+   tx_buffer(port, gps_initstr, (uint16_t*)&gps_len);
+   while( gps_len );
+   // */
 
    gps_port = port;
 }
@@ -49,6 +71,13 @@ void gps_spinOnce(void) {
             gps_pub.append(lon);
             // TODO: fill in rest of GPS message
             gps_pub.finish();
+         }
+
+         if( gps_pub2.reset() ) {
+            gps_pub2.append(lat);
+            gps_pub2.append(lon);
+            // TODO: fill in rest of GPS message
+            gps_pub2.finish();
          }
       } 
    }
--- publish.h
+++ publish.h
@@ -19,21 +19,20 @@ extern uint8_t pub_enable;
 
 // experimental publisher class; wraps the Packet class
 // targeted to my AVR
-template<uint8_t SZ>
+template<uint8_t SZ, uint8_t SER = BRAIN>
 class Publisher {
    private:
-      uint16_t brain_sz;
+      uint16_t sz;
       char buffer[SZ];
       Packet p;
 
    public:
       Publisher(char topic) : p(topic, (uint8_t)SZ, buffer) {
-         brain_sz = 0;
-         //bt_sz = 0;
+         sz = 0;
       }
 
       int8_t reset() {
-         if( brain_sz > 0 ) {
+         if( sz > 0 ) {
             led_on();
             return 0;
          } else {
@@ -45,9 +44,8 @@ class Publisher {
       void finish() {
          p.finish();
          if( pub_enable ) {
-            brain_sz = p.outsz();
-            tx_buffer(BRAIN, (const uint8_t *)buffer, &brain_sz);
-            //tx_buffer(BRAIN, (const uint8_t *)p.outbuf(), &brain_sz);
+            sz = p.outsz();
+            tx_buffer(SER, (const uint8_t *)buffer, &sz);
          }
       }
 
